dc4351d2552b9dab2ee8c3b2f8ef2a5df9b20f35
yunfeiyangbuaa <43897640+yunfeiyangbuaa@users.noreply.github.com>
2019-01-02 10:52:15 +0800

    Update BBS04.go

--- BBS04.go
+++ BBS04.go
@@ -221,7 +221,7 @@ func   (priv  *PrivateKey)open(sig  *Sig)*pbc.Element{
 	temp1:=priv.pairing.NewG1().PowZn(sig.t1,priv.xi1)
 	temp2:=priv.pairing.NewG1().PowZn(sig.t2,priv.xi2)
 	temp3:=priv.pairing.NewG1().Mul(sig.t3,priv.pairing.NewG1().Invert(priv.pairing.NewG1().Mul(temp1,temp2)))
-    fmt.Println(temp3.String())
+        fmt.Println(temp3.String())
 	return   temp3
 }
 
@@ -229,37 +229,6 @@ func   (priv  *PrivateKey)open(sig  *Sig)*pbc.Element{
 
 
 
-func  main(){
-	params:=pbc.GenerateA(160,512)
-	pairing:=params.NewPairing()
-	g1:=pairing.NewG1().Rand()
-	g2:=pairing.NewG2().Rand()
-	priv:=GenerateGroup(g1,g2 ,pairing)
-    //genarate  new  member
-	member:=priv.NewMember()
-	//generate  new  cert
-	cert:=priv.Cert(member.u_)
-	fmt.Println(cert.A.String())
 
-	member1:=priv.NewMember()
-	cert1:=priv.Cert(member1.u_)
-	fmt.Println(cert1.A.String())
-
-	//verify  cert
-	member.Verify_cert(cert)
-	member1.Verify_cert(cert1)
-	//generate   mima
-	c1:=pairing.NewG1().Rand()
-	c2:=pairing.NewG1().Rand()
-	c3:=pairing.NewG1().Mul(c1,c2)
-	//generate  signature
-	sig:=member.Sign(cert,c1,c2,c3)
-	sig1:=member1.Sign(cert1,c1,c2,c3)
-	//verify    signature
-	priv.Group.Verify_sign(sig,member.h_)
-	priv.Group.Verify_sign(sig1,member1.h_)
-	priv.open(sig)
-	priv.open(sig1)
-}
 
 
